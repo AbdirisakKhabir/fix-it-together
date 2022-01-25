1b7805f3c7b092a6f43b7b37f9a23e7dee9b9370
zakaria <zakariaabdi104@gmail.com>
2022-01-25 16:36:13 -0500

    clenaned

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      i cleaned all files.
+      maanta waxaan ku dareynaa file lasha cusub
+
+    </div>
+  );
+}
+
+export default App;
